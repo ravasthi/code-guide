681696092241465d64a709aa2ff78a1344618c2c
Richa Avasthi <ravasthi@gmail.com>
2016-01-20 15:42:11 -0800

    Various improvements
    Why:
    
    * Cleanup for improved readability
    * Addressing review comments
    
    This change addresses the need by:
    
    * Marking some JS keywords up as code in the text side
    * Adding in clarifications about variable names to the code snippets

--- _includes/js/names.js
+++ _includes/js/names.js
@@ -18,3 +18,15 @@ function Vehicle() {
 }
 
 var vehicle = new Vehicle();
+
+/*
+** Use the $ prefix to indicate that the variable contains
+** a jQuery object
+*/
+var $listItems = $('.list-item');
+
+/*
+** Use all caps for global variables and things you wish
+** were constant
+*/
+var PST_OFFSET = -8;
